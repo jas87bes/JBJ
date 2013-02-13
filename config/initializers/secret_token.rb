a045b104dd0094439eeb83be50e4c28d394eb133
Jasmin Besic <jasmin@optyn.com>
2013-02-13 10:31:28 -0600

    Firs commit
    First app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JBJ::Application.config.secret_token = '22bb53bc025e2f922396956faec541a86e4a56e1803e771106d7cdeb88f1fd15c90a3ee0d458cdb74d6a7f0123ef6725b1e14ce049ae2a962843949aa3e7e166'
